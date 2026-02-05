5f44ca7c4facfea02e7a8f6048bd44f3db7e02d1
Samar <samarkamat88@gmail.com>
2026-02-05 22:41:57 +0530

    Trying BCE + Dice loss for this
    Weight decay did not work, maybe loss helps?

--- scripts/train.py
+++ scripts/train.py
@@ -72,17 +72,17 @@ def train_one_epoch(model, loader, optimizer, criterion_bce, criterion_dice, dev
 
         # Main Loss
         loss_bce_main = criterion_bce(logits, masks)
-        # loss_dice_main = criterion_dice(logits, masks)
-        # loss_main = 0.5 * loss_bce_main + 0.5 * loss_dice_main
+        loss_dice_main = criterion_dice(logits, masks)
+        loss_main = 0.5 * loss_bce_main + 0.5 * loss_dice_main
         loss_main = loss_bce_main
 
         # Auxiliary Loss
         loss_bce_aux = criterion_bce(aux_logits, masks)
-        # loss_dice_aux = criterion_dice(aux_logits, masks)
-        # loss_aux = 0.5 * loss_bce_aux + 0.5 * loss_dice_aux
+        loss_dice_aux = criterion_dice(aux_logits, masks)
+        loss_aux = 0.5 * loss_bce_aux + 0.5 * loss_dice_aux
         loss_aux = loss_bce_aux
 
-        # Auxiliary loss toggle (not using it currently)
+        # Auxiliary loss toggle
         loss = loss_main + (0.5 * loss_aux)
 
         loss.backward()
@@ -111,8 +111,8 @@ def validate(model, loader, criterion_bce, criterion_dice, device):
             logits = outputs['out']
 
             loss_bce = criterion_bce(logits, masks)
-            # loss_dice = criterion_dice(logits, masks)
-            # loss = 0.5 * loss_bce + 0.5 * loss_dice
+            loss_dice = criterion_dice(logits, masks)
+            loss = 0.5 * loss_bce + 0.5 * loss_dice
             loss = loss_bce
 
             running_loss += loss.item()
@@ -157,7 +157,7 @@ def main():
             print(f"Using {torch.cuda.device_count()} GPUs")
             model = nn.DataParallel(model)
 
-    optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'], weight_decay=1e-4)
+    optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
     
     criterion_bce = nn.BCEWithLogitsLoss()
     criterion_dice = DiceLoss()
